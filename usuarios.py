66b373ad581f2aa33d101e68ea5fd5a4b4baf228
kaue-aps <kaue.silva6202@gmail.com>
2026-08-08 00:03:00 +0000

    renomeando arquivo de dados

--- usuarios.py
+++ usuarios.py
@@ -1,15 +1,15 @@
-arquivo = open("usuario.txt", "a")
+arquivo = open("banco_dados.txt", "a")
 
 def cadastrar():
     while True:
         usuario = str(input("usuário: "))
         senha = str(input("Senha: "))
 
-        with open("usuario.txt", "r") as arquivo:
+        with open("banco_dados.txt", "r") as arquivo:
             for linha in arquivo:
                 separador = linha.split(",")
                 if usuario in separador:
                     print("Erro")
                     return(cadastrar)
-        with open("usuario.txt", "a") as arquivo:
+        with open("banco_dados.txt", "a") as arquivo:
             arquivo.write(f"{usuario},{senha}\n")
