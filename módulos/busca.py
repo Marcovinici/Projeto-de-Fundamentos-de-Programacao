400895691adcb7fbe1801a56ebaeb01a5898be3b
Marcos Vinicius <marcosvinicius.dev.tb@gmail.com>
2026-08-12 21:41:41 -0300

    Adicionando função que valida coordenadas e que uma função que cria a rota

--- módulos/busca.py
+++ módulos/busca.py
@@ -11,3 +11,20 @@ def printMatriz(matriz, rota):
             else:
                 print(coluna, end="")
         print()
+        
+# Função para construir a rota
+def cria_rota(inicio, destino_encontrado, pais):
+    rota = []
+    while destino_encontrado != inicio:
+        rota.append(destino_encontrado)
+        destino_encontrado = pais[destino_encontrado]
+    return rota
+
+# Função validadora de coordenada(caminho):
+# Checa se posição na matriz faz parte da estrada e se já não foi visitada
+def validacao(linha, coluna, n, m, matriz, visitado):
+    
+    return linha >= 0 and linha < n and \
+        coluna >= 0 and coluna < m and \
+        matriz[linha][coluna] != '#' and \
+        not visitado[linha][coluna]
