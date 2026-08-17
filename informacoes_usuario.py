76517d63585d7601942556e6dc5c2678cfd204f8
kaue-aps <kaue.silva6202@gmail.com>
2026-08-17 14:24:24 -0300

    A função exibir_informacoes_usuario agora retorna valores importantes a serem usadas pelas ações do submenu

--- informacoes_usuario.py
+++ informacoes_usuario.py
@@ -2,7 +2,8 @@
 # Seção de exibição de informações do usuário, como data de criação da conta, histórico de rotas, rotas favoritas e outras informações pertinentes.
 
 # Falta: Implementar a lógica para exibir o histórico de rotas e as rotas favoritas.
-# falta: Fazer menu de 
+
+# Opções disponíveis ao selecionar no submenu de 5. Informações do usuário
 # 1 - Limpar histórico
 # 2 - Modificar senha - Lógica já implementada
 # 3- Excluir conta
@@ -24,12 +25,12 @@ def exibir_informacoes_usuario(usuario):
         print(f"Usuário: {usuario}")
         print(f"Data de criação da conta: {info_usuario.get('data', 'Não disponível')}")
 
+        return cadastro, info_usuario 
+
         # Esperando o desenvolvimento de funcionalidades futuras,
         # como histórico de rotas e rotas favoritas.
         # print(f"Histórico de rotas: {info_usuario.get('historico_rotas', 'Não disponível')}")
         # print(f"Rotas favoritas: {info_usuario.get('rotas_favoritas', 'Não disponível')}")
-
-        redefinir_senha(cadastro, info_usuario)
 
     else:
         print("Usuário não encontrado.")
