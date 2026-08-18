1bac5e9e7d3a82ad675b0d4cc10aa5c2c2b3c58b
kaue-aps <kaue.silva6202@gmail.com>
2026-08-18 19:23:56 -0300

    retirando função de contar rota mais pesquisada

--- relatorio.py
+++ relatorio.py
@@ -14,14 +14,9 @@ def contar_cadastros_hoje():
             quantidade += 1
     return quantidade
 
-def contar_rota_mais_pesquisada():
-    pass
-
-
 
 def exibir_relatorio():
     print(f'[bold blue]{' RELATÓRIO DO ADMINISTRADOR ':=^50}[/]')
     print(f'Quantidade de usuários cadastrados no sistema: {contar_usuarios()}')
     print(f'Quantidade de cadastros realizados hoje: {contar_cadastros_hoje()}')
-    print(f'Rota mais pesquisada: {contar_rota_mais_pesquisada}')
     print(f'[bold blue]{'=' * 50}[/]')
