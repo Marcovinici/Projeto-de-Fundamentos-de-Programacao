d49c6cd943a971d2afdfb09d1ca241aa05bc1164
kaue-aps <kaue.silva6202@gmail.com>
2026-08-19 17:37:47 -0300

    adicionando orientações iniciais ao usuário

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from menus_e_submenus import menu, menu_admin, limpar_terminal
 
 while True:
 	limpar_terminal()
+	print(f"\n[bold blue]{' BEM VINDO AO MOBCLI ':=^50}[/]")
+	print('Por favor, insira suas credenciais.')
 	# Pede usuário e senha
 	usuario, senha = tela_de_login()
 
 	# Verifica login e realiza novas tentativas ou cadastro, se necessário
 	usuario = cadastrar_ou_tentar_novamente(usuario, senha)
 
-    # Leva ao menu principal caso o login seja realizado
+    # Leva ao menu principal ou ao menu do admin caso o login seja realizado
 	if usuario:
 		limpar_terminal()
 		cadastro = carregar_cadastro()
